c6add97f94e9d36fec8b0bcd01f728f5ff6720b0
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2026-04-02 19:44:25 +0300

    first commit

---
+++ main.kt
@@ -0,0 +1,26 @@
+// Kotlin -> Javanın moderni
+// Daha az kodla aynı işi yapabilen versiyon.
+
+// Entrypoint => Giriş noktası
+fun main() 
+{
+    // Scope => Kapsama alanı 
+    println("Hello")
+
+    // Değişkenler
+    // Programın akışında tanımlanan ramde tutulan ve tanımlanan isimle erişilebilen, değeri değişebilen değer(ler).
+    var a: Int = 5 // a ismine 5 değerini atadık.
+    println(a)
+    // int => integer => tam sayı
+
+    var name: String = "Halit" // String (metinsel)
+    println(name)
+
+    val b: Int = 10 // Immutable => Değiştirilemez
+    println(b)
+
+    a = 6
+    println(a)
+    b=11
+    println(b)
+}
