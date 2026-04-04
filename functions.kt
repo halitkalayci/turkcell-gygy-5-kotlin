6841491d1cbbae394a915a63aeee420ddac01f5d
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2026-04-04 10:36:23 +0300

    named args & default value

--- functions.kt
+++ functions.kt
@@ -2,11 +2,16 @@ fun main() {
     selamVer()
     selamVer2("Ahmet")
 
-    var not1 = notHesapla(85) // ekrana yaz
+    var not1 = notHesapla(85,"Halit") // ekrana yaz
     var not2 = notHesapla(72) // mail atmak
-    var not3 = notHesapla(30) // sms atmak
+    var not3 = notHesapla(30, "Mehmet") // sms atmak
+
+    // named args
+    var not4 = notHesapla(isim="Ayse", not=95)
 
     println(not1)
+    println(not2)
+    println(not4)
     // not2'yi email at
     // not3'ü sms at
 }
@@ -27,16 +32,18 @@ fun selamVer2(isim:String) {
 
 // Fonksiyonlar bir şey döndürmeyen (void,Unit) olabilirler,
 // bir şey döndürebilirler (return) olabilirler.
-fun notHesapla(not:Int): String {
+
+// default parameter => fonksiyon çağırılırken parametre verilmezse, default olarak atanacak değerdir.
+fun notHesapla(not:Int, isim:String="Ogrenci"): String {
     return when(not){
-        in 90..100 -> "AA"
-        in 80..89 -> "BA"
-        in 70..79 -> "BB"
-        in 60..69 -> "CB"
-        in 50..59 -> "CC"
-        in 40..49 -> "DC"
-        in 30..39 -> "DD"
-        in 0..29 -> "FF"
-        else -> "Geçersiz not"
+        in 90..100 -> "$isim AA"
+        in 80..89 -> "$isim BA"
+        in 70..79 -> "$isim BB"
+        in 60..69 -> "$isim CB"
+        in 50..59 -> "$isim CC"
+        in 40..49 -> "$isim DC"
+        in 30..39 -> "$isim DD"
+        in 0..29 -> "$isim FF"
+        else -> "$isim Geçersiz not"
     }
 }
